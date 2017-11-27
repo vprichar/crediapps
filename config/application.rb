2c8aec68312da9f221fa81e7713dc1461d9ef91b
Ricardo <vprichar@gmail.com>
2017-11-26 20:23:23 -0700

    config.time_zone = "America/Mazatlan"

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ Bundler.require(*Rails.groups)
 
 module AngularRails
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
+    config.time_zone = "America/Mazatlan"
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
